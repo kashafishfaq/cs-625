da38e492ca954432b07516931da35ee36f970cd1
kashafishfaq <kashafishfaq45@gmail.com>
2019-02-28 00:29:35 +0500

    error resolved file

--- WaterSong.java
+++ WaterSong.java
@@ -15,13 +15,14 @@ System.out.println(waterNum + " " + word + " of water");
 System.out.println("take one down.");
 System.out.println("pass it around.");
 waterNum =waterNum -1;
+
 if (waterNum > 0)
 {
  System.out.println(waterNum + " " + word + " of water on the wall");
 }
-esle {
+else{
 System.out.println("No more bottles");
 }
 }
 }
-}}
+}
